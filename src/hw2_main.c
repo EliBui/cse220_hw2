9a1edd744d6e4265fea8c57fe9dccf535f5b131a
EliBui <91813317+EliBui@users.noreply.github.com>
2024-03-02 03:34:49 +0000

    part1

--- src/hw2_main.c
+++ src/hw2_main.c
@@ -9,14 +9,81 @@
 #include <unistd.h>
 #include <errno.h> 
 
-void unUsedVar(long cParameters[], long pParameters[], char *rMessage, char *rPath_to_front, long rFont_size,long rRow, long rCol) {
-    printf("cParameters: %ld\n", *cParameters);
-    printf("pParameters: %ld\n", *pParameters);
-    printf("rMessage: %s\n", rMessage);
-    printf("rPath_to_front: %s\n", rPath_to_front);
-    printf("rFont_size: %ld\n", rFont_size);
-    printf("rRow: %ld\n", rRow);
-    printf("rCol: %ld\n", rCol);
+// void unUsedVar(long cParameters[], long pParameters[], char *rMessage, char *rPath_to_front, long rFont_size,long rRow, long rCol) {
+//     printf("cParameters: %ld\n", *cParameters);
+//     printf("pParameters: %ld\n", *pParameters);
+//     printf("rMessage: %s\n", rMessage);
+//     printf("rPath_to_front: %s\n", rPath_to_front);
+//     printf("rFont_size: %ld\n", rFont_size);
+//     printf("rRow: %ld\n", rRow);
+//     printf("rCol: %ld\n", rCol);
+// }
+
+bool processCInputs(long *cParameters, char *cTxt) {
+    char *token = strtok(cTxt, ",");
+    int index = 0;
+    cParameters[index++] = strtol(token, NULL, 10);
+
+    while((token = strtok(NULL, ",")) != NULL && index > 3) {
+        cParameters[index++] = strtol(token, NULL, 10);
+    }
+
+    if(index != 4 || token != NULL) {
+        return true;
+    }
+    return false;
+}
+
+bool processPInputs(long *pParameters, char *pTxt) {
+    char *token = strtok(pTxt, ",");
+    int index = 0;
+
+    pParameters[index++] = strtol(token, NULL, 10);
+
+    while((token = strtok(NULL, ",")) != NULL && index > 1) {
+        pParameters[index++] = strtol(token, NULL, 10);
+    }
+
+    if(index != 2 || token != NULL) {
+        return true;
+    }
+
+    return false;
+}
+
+bool processRInputs(char *rMessage, char *rPath_to_front, long rFont_size, long rRow, long rCol, char *rTxt) {
+    char *token;
+    rMessage = strtok(rTxt, ",");
+
+    if((token = strtok(NULL, ",")) == NULL) {
+        return true;
+    }
+    rPath_to_front = token;
+    
+    if((token = strtok(NULL, ",")) == NULL) {
+        return true;
+    }
+    rFont_size = strtol(token, NULL, 10);
+
+    if((token = strtok(NULL, ",")) == NULL) {
+        return true;
+    }
+    rRow = strtol(token, NULL, 10);
+
+    if((token = strtok(NULL, ",")) == NULL) {
+        return true;
+    }
+    rCol = strtol(token, NULL, 10);
+
+    if((token = strtok(NULL, ",")) == NULL) {
+        return true;
+    }
+    (void) rMessage;
+    (void) rPath_to_front;
+    (void) rFont_size;
+    (void) rRow;
+    (void) rCol;
+    return false;
 }
 
 int main(int argc, char **argv) {
@@ -24,12 +91,13 @@ int main(int argc, char **argv) {
     extern int optind;
     int command, iCount=0, oCount=0, cCount=0, pCount=0, rCount=0, invalidCount=0;
     char *iTxt, *oTxt, *cTxt, *pTxt, *rTxt;
+    bool UA = false, DA = false, IFM = false, OFU = false, CAM = false, CAI = false, PAI = false, RAI = false;
 
     while((command = getopt(argc, argv, "i:o:c:p:r:")) != -1) {
-        printf("c: %d\n", command);
-        printf("optarg: %s\n", optarg);
-        printf("optind: %d\n", optind);
-        printf("optarg first ele: %c\n", optarg[0]);
+        // printf("c: %d\n", command);
+        // printf("optarg: %s\n", optarg);
+        // printf("optind: %d\n", optind);
+        // printf("optarg first ele: %c\n", optarg[0]);
         if(optarg[0] == '-') {
             return MISSING_ARGUMENT;
         }
@@ -68,93 +136,86 @@ int main(int argc, char **argv) {
     }
 
     if(invalidCount > 0) {
+        UA = true;
         return UNRECOGNIZED_ARGUMENT;
     }
 
     if(iCount > 1 || oCount > 1 || cCount > 1 || pCount > 1 || rCount > 1) {
+        DA = true;
         return DUPLICATE_ARGUMENT;
     }
 
     FILE *fpIn;
     if((fpIn = fopen(iTxt, "r")) == NULL) {
+        IFM = true;
         return INPUT_FILE_MISSING;
     }
 
     FILE *fpOut;
     if((fpOut = fopen(oTxt, "w")) == NULL) {
+        printf("%s\n", oTxt);
+        OFU = true;
         return OUTPUT_FILE_UNWRITABLE;
     }
 
     if(pCount > 0 && cCount == 0) {
+        CAM = true;
         return C_ARGUMENT_MISSING;
     }
 
     long cParameters[4];
-    char *token = strtok(cTxt, ",");
-    int index = 0;
-    char *endPtr;
-    cParameters[index++] = strtol(token, &endPtr, 10);
-    if(endPtr != NULL) {
-        return UNRECOGNIZED_ARGUMENT;
-    }
-
-    while((token = strtok(NULL, ",")) != NULL && index > 3) {
-        cParameters[index++] = strtol(token, &endPtr, 10);
-        if(endPtr != NULL) {
-            return UNRECOGNIZED_ARGUMENT;
+    if(cTxt != NULL) {
+        CAI = processCInputs(cParameters, cTxt);
+        if(CAI) {
+            return C_ARGUMENT_INVALID;
         }
-    }
-
-    if(index != 4 || token != NULL) {
-        return C_ARGUMENT_INVALID;
     }
     
     long pParameters[2];
-    token = strtok(pTxt, ",");
-    index = 0;
-    pParameters[index++] = strtol(token, &endPtr, 10);
-    if(endPtr != NULL) {
-        return UNRECOGNIZED_ARGUMENT;
+    if(pTxt != NULL) {
+        PAI = processPInputs(pParameters, pTxt);
+        if(PAI) {
+            return P_ARGUMENT_INVALID;
+        }
     }
-
-    while((token = strtok(NULL, ",")) != NULL && index > 1) {
-        cParameters[index++] = strtol(token, &endPtr, 10);
-        if(endPtr != NULL) {
-            return UNRECOGNIZED_ARGUMENT;
+    
+    
+    if(rTxt != NULL) {
+        char *rMessage = "";
+        char *rPath_to_front = "";
+        long rFont_size = 0;
+        long rRow = 0;
+        long rCol = 0;
+        RAI = processRInputs(rMessage, rPath_to_front, rFont_size, rRow, rCol, rTxt);
+        if(RAI) {
+            return R_ARGUMENT_INVALID;
+        }
+        FILE *fpFonts;
+        if((fpFonts = fopen(oTxt, "r")) == NULL) {
+            return R_ARGUMENT_INVALID;
         }
     }
 
-    if(index != 2 || token != NULL) {
+
+    //unUsedVar( cParameters,  pParameters,  rMessage,  rPath_to_front,  rFont_size, rRow, rCol);
+    
+    if(UA == true) {
+        return UNRECOGNIZED_ARGUMENT;
+    } else if(DA == true) {
+        return DUPLICATE_ARGUMENT;
+    } else if(IFM == true) {
+        return INPUT_FILE_MISSING;
+    } else if(OFU == true) {
+        return OUTPUT_FILE_UNWRITABLE;
+    } else if(CAM == true) {
+        return C_ARGUMENT_MISSING;
+    } else if(CAI == true) {
+        return C_ARGUMENT_INVALID;
+    } else if(PAI == true) {
         return P_ARGUMENT_INVALID;
-    }
-    
-    char *rMessage = strtok(rTxt, ",");
-
-    if((token = strtok(NULL, ",")) == NULL) {
+    } else if(RAI == true) {
         return R_ARGUMENT_INVALID;
+    } else {
+        return 0;
     }
-    char *rPath_to_front = token;
-    
-    if((token = strtok(NULL, ",")) == NULL) {
-        return R_ARGUMENT_INVALID;
-    }
-    long rFont_size = strtol(token, NULL, 10);
-
-    if((token = strtok(NULL, ",")) == NULL) {
-        return R_ARGUMENT_INVALID;
-    }
-    long rRow = strtol(token, NULL, 10);
-
-    if((token = strtok(NULL, ",")) == NULL) {
-        return R_ARGUMENT_INVALID;
-    }
-    long rCol = strtol(token, NULL, 10);
-
-    if((token = strtok(NULL, ",")) == NULL) {
-        return R_ARGUMENT_INVALID;
-    }
-
-
-    unUsedVar( cParameters,  pParameters,  rMessage,  rPath_to_front,  rFont_size, rRow, rCol);
-    return 0;
 }
